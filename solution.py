e9d33b945a7c0032b0b3c21301089d5990a7cbac
krishnananda255-dotcom <krishnananda255@gmail.com>
2026-02-17 20:39:45 +0530

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,3 +1,6 @@
-def add_numbers(a,b):
-  #TODO 
-  pass
+num = int(input("Enter an integer: ")) 
+sum_digits = 0 
+while num > 0:
+  digit = num % 10 
+  sum_digits += digit num //= 10
+print("Sum of digits:", sum_digits)
